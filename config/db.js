eca9456f057988c085bdb5e2b02edc4a1888a73d
Nathan Renner <nathanrenner68@gmail.com>
2022-04-07 20:10:53 -0500

    add Post class with methods, and Controller method

--- config/db.js
+++ config/db.js
@@ -1,10 +1,14 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
+
+let sql = "SELECT * FROM posts;";
 
 module.exports = pool.promise();
---
+++ models/Post.js
@@ -0,0 +1,44 @@
+const db = require('../config/db');
+class Post {
+    constructor(title, body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    save() {
+        let date = new Date();
+        let year = date.getFullYear();
+        let month = date.getMonth() + 1;
+        let day = date.getDate();
+
+        let createdAtDate = `${year}-${month}-${day}`;
+
+        let sql = `
+        INSERT INTO posts(
+            title,
+            body,
+            createdAt
+        )
+        VALUES(
+            '${this.title}',
+            '${this.body}',
+            '${createdAtDate}'
+        )
+        `;
+
+        return db.execute(sql);
+    }
+
+    static findAll(){
+        let sql = "SELECT * FROM posts;";
+        return db.execute(sql);
+    }
+
+    static findById(id){
+        let sql = `SELECT * FROM posts WHERE id = ${id};`;
+        return db.execute(sql);
+    }
+}
+
+module.exports = Post;
+
